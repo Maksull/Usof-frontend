2774d851d1fb13c7ffd613b218b2fd4d37467f71
Maksull <49917345+Maksull@users.noreply.github.com>
2024-11-18 14:37:23 +0200

    Add models (#2)

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+export { Category } from "./Category";
+export { Comment } from "./Comment";
+export { Like, LikeType } from "./Like";
+export { Post, PostStatus } from "./Post";
+export { User, UserRole } from "./User";
